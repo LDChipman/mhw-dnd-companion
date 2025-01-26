eed0cc10be399f997ded0fee975699c316e0af5f
LDChipman <chipman.liam@gmail.com>
2025-01-26 15:56:30 -0600

    feat: added test to check that the builder applies all the fields set properly after instantiation

--- __tests__/MonsterPart-test.tsx
+++ __tests__/MonsterPart-test.tsx
@@ -22,6 +22,42 @@ describe("Monster Part Building", () => {
 		expect(PART.getDamageTaken).toBe(PROPERLY_BUILT_BASIC_MONSTER_PART.damageTaken);
 	});
 
+	test("Monster Part Builder Correctly Sets All Fields", () => {
+		const TEST_HITZONES = generateHitzonesFromArray([
+			generateHitzone(damageTypes.bludgeoning, 1, 3),
+			generateHitzone(damageTypes.slashing, 5, 7),
+			generateHitzone(damageTypes.piercing, 9, 11),
+			generateHitzone(damageTypes.fire, 13, 15),
+			generateHitzone(damageTypes.electricity, 17, 19),
+			generateHitzone(damageTypes.cold, 21, 23),
+			generateHitzone(damageTypes.acid, 25, 27),
+			generateHitzone(damageTypes.sonic, 29, 31),
+			generateHitzone(damageTypes.positive, 33, 35),
+			generateHitzone(damageTypes.negative, 37, 39),
+			generateHitzone(damageTypes.force, 41, 43),
+			generateHitzone(damageTypes.holy, 45, 47),
+			generateHitzone(damageTypes.unholy, 49, 51)
+		]);
+		
+		const PART = new MonsterPart.Builder()
+			.setName("Test Name")
+			.setPartBreakThreshold(5)
+			.setPartBreakThresholdIncrease(8)
+			.setTimesPartCanBeBroken(13)
+			.setTimesPartHasBeenBroken(17)
+			.setDamageTaken(21)
+			.setHitzone(TEST_HITZONES)
+			.build();
+
+		expect(PART.getName).toBe("Test Name");
+		expect(PART.partBreakThreshold).toBe(5);
+		expect(PART.partBreakThresholdIncrease).toBe(8);
+		expect(PART.timesPartCanBeBroken).toBe(13);
+		expect(PART.timesPartHasBeenBroken).toBe(17);
+		expect(PART.damageTaken).toBe(21);
+		expect(PART.getHitzones).toEqual(TEST_HITZONES);
+	})
+
 	test("Properly applies damage to part", () => {
 		const PART = new MonsterPart.Builder()
 			.setPartBreakThreshold(20)
@@ -31,22 +67,22 @@ describe("Monster Part Building", () => {
 
 		PART.applyDamageToPart(19);
 		
-		expect(PART.getDamageTaken()).toBe(19);
-		expect(PART.getTimesPartHasBeenBroken()).toBe(0);
+		expect(PART.getDamageTaken).toBe(19);
+		expect(PART.getTimesPartHasBeenBroken).toBe(0);
 
 		PART.applyDamageToPart(1);
 
-		expect(PART.getDamageTaken()).toBe(20);
-		expect(PART.getTimesPartHasBeenBroken()).toBe(1);
+		expect(PART.getDamageTaken).toBe(20);
+		expect(PART.getTimesPartHasBeenBroken).toBe(1);
 
 		PART.applyDamageToPart(40);
 
-		expect(PART.getDamageTaken()).toBe(60);
+		expect(PART.getDamageTaken).toBe(60);
 		expect(PART.getTimesPartHasBeenBroken).toBe(3);
 
 		PART.applyDamageToPart(500);
 
-		expect(PART.getDamageTaken()).toBe(560);
+		expect(PART.getDamageTaken).toBe(560);
 		expect(PART.getTimesPartHasBeenBroken).toBe(3);
 	});
 
