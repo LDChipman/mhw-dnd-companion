14e3cbafbb1fb20817ad54b3ffca6b15369dbb5c
LDChipman <chipman.liam@gmail.com>
2025-02-01 18:20:48 -0600

    feat: added test descriptions for as of yet implemented Monster Class

---
+++ __tests__/Monster-test.tsx
@@ -0,0 +1,16 @@
+describe("Monster", () => {
+
+	test.todo("Monster Properly Changes states");
+
+	test.todo("Monster Properly Sets all of its Fields during instantiation");
+
+	test.todo("Monster Properly Filters Its List of Attacks to currently usable attacks");
+
+	test.todo("Monster Properly Changes States Upon Hitting Certain Health Thresholds");
+
+	test.todo("Monster Properly Applies Damage to its Health Bar when receiving an attack to one of its parts");
+
+	test.todo("Monster Stamina Properly Updates when Using an attack and when taking damage");
+
+	test.todo("Monster Properly Updates Status Conditions when dealt status damage from a specialized part")
+})
