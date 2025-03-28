19c49999ee71ed7a795a6627fed07a9c18e53869
LDChipman <chipman.liam@gmail.com>
2025-03-28 11:48:52 -0600

    feat: added more to the implementation of the test to check that the monster object properly filters its available attacks

--- __tests__/Monster-test.tsx
+++ __tests__/Monster-test.tsx
@@ -67,8 +67,8 @@ describe("Monster", () => {
 		const SPECIAL_STATES = [generateMonsterState("Flying"), generateMonsterState("Grounded")];
 
 		const ATTACKS: Array<MonsterAttack> = [];
+		ATTACKS.push(new MonsterAttack("Wing Blast", ["Knocks all Medium and Smaller creatures back 15ft in a 20ft cone. DC 15 Strength check to resist, failing by 5 or more causes the target to fall prone", "Knocks all Medium and Smaller creatures back 10ft in a 15ft cone. DC 10 Strength check to resist, failing by 5 or more causes the target to fall prone"], [ENERGY_LEVELS.at(0)!, SPECIAL_STATES.at(0)!]));
 		ATTACKS.push(new MonsterAttack("Bite", ["Deals 1d8+5 piercing damage"], [ENERGY_LEVELS.at(0)!, SPECIAL_STATES.at(1)!]));
-		ATTACKS.push(new MonsterAttack("Wing Blast", ["Knocks all Medium and Smaller creatures back 15ft in a 20ft cone. DC 15 Strength check to resist, failing by 5 or more causes the target to fall prone", "Knocks all Medium and Smaller creatures back 10ft in a 15ft cone. DC 10 Strength check to resist, failing by 5 or more causes the target to fall prone"], [ENERGY_LEVELS.at(1)!, SPECIAL_STATES.at(0)!]));
 		ATTACKS.push(new MonsterAttack("Claw Swipe", ["Deals 1d6+5 slashing damage", "Deals 1d4+3 slashing damage"], [ENERGY_LEVELS.at(0)!, ENERGY_LEVELS.at(1)!, SPECIAL_STATES.at(1)!]))
 
 		const PARTS: Array<MonsterPart> = [new MonsterPart.Builder()
@@ -76,7 +76,15 @@ describe("Monster", () => {
 
 		const MONSTER = new Monster(NAME, ENERGY_LEVELS, SPECIAL_STATES, ATTACKS, PARTS);
 
-		expect(MONSTER.getUsableAttacks).toHaveLength(1);
+		expect(MONSTER.getAvailableAttacks).toHaveLength(1);
+
+		MONSTER.setCurrentSpecialState(MONSTER.getSpecialStates.at(1));
+
+		expect(MONSTER.getAvailableAttacks).toHaveLength(2);
+
+		MONSTER.setCurrentEnergyLevel(MONSTER.getEnergyLevels.at(1));
+
+		expect(MONSTER.getAvailableAttacks).toHaveLength(1);
 
 	});
 
